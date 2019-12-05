4db090e6137facd86607da095ee2b0f547393a52
Dean McHugh <dean.mchugh1988@gmail.com>
2019-12-05 20:22:12 +1000

    secret keys

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import fetch from '../helpers/fetchData'
 
 export default function home() {
     return (
-        <div>
-            <button onClick={(event: any) => fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo')}>fetch</button>
+        <div id='mainPage'>
+            <h1>market tracker</h1>
+            <button onClick={(event: MouseEvent) => fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey={process.env.API_KEY}`)}>fetch</button>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import HomePage from './components/home'
 
 function Main() {
